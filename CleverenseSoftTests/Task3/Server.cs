07cb902f241fa05feb38a3ff2610452461dd8aad
John <John@DESKTOP-70PP7PL>
2023-11-27 21:02:51 +0300

    Tas 3 - synchronized server is added

---
+++ CleverenseSoftTests/Task3/Server.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace CleverenseSoftTests.Task3
+{
+    internal static class Server
+    {
+        private static Int32 _count;
+        private static ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+        public static Int32 GetCount()
+        {
+            try
+            {
+                _lock.EnterReadLock();
+                return _count;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        
+        }
+
+        public static void AddToCount(Int32 value)
+        {
+
+            _lock.EnterWriteLock();
+            _count += value;
+            _lock.ExitWriteLock();
+        }
+
+    }
+}
