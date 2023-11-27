818b9e931a956f90f559259ede13dce6d5650574
John <John@DESKTOP-70PP7PL>
2023-11-27 19:38:42 +0300

    Task 2 matrix service is created with clockwise function

---
+++ CleverenseSoftTests/Task2/IMatrixService.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace CleverenseSoftTests.Task2
+{
+    internal interface IMatrixService
+    {
+        T[] GetMatrixCounterWiseArray<T>(T[,] matrix);
+    }
+}
---
+++ CleverenseSoftTests/Task2/MatrixService.cs
@@ -0,0 +1,61 @@
+﻿
+namespace CleverenseSoftTests.Task2
+{
+    internal class MatrixService : IMatrixService
+    {
+        public T[] GetMatrixCounterWiseArray<T>(T[,] matrix)
+        {
+            List<T> listOElements = new List<T>();
+
+            Int32 i, k = 0, l = 0;
+            Int32 m = matrix.GetLength(0);
+            Int32 n = matrix.GetLength(1);
+
+            Int32 count = 0;
+
+            Int32 total = matrix.Length;
+
+            while (k < m && l < n)
+            {
+                if (count == total)
+                    break;
+
+                for (i = k; i < m; ++i)
+                {
+                    listOElements.Add(matrix[i, l]);
+                    count++;
+                }
+                l++;
+
+                if (count == total)
+                    break;
+
+                for (i = l; i < n; ++i)
+                {
+                    listOElements.Add(matrix[m - 1, i]);
+                    count++;
+                }
+                m--;
+
+                if (count == total)
+                    break;
+
+                if (k < m)
+                {
+                    for (i = m - 1; i >= k; --i)
+                    {
+                        listOElements.Add(matrix[i, n - 1]);
+                        count++;
+                    }
+                    n--;
+                }
+
+                if (count == total)
+                    break;
+            }
+
+            return listOElements.ToArray();
+        }
+
+    }
+}
