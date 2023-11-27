c83ed398c3c4495c1767271e9ddad1911141ebe6
John <John@DESKTOP-70PP7PL>
2023-11-27 19:01:47 +0300

    Task 1 compressor is created

--- CleverenseSoftTests/Program.cs
+++ CleverenseSoftTests/Program.cs
@@ -5,11 +5,5 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-
-        Compressor c = new Compressor();
-        string a = c.Compression("aaacbbd");
-        string b = c.Decompression(a);
-        Console.WriteLine(a);
-
     }
 }
